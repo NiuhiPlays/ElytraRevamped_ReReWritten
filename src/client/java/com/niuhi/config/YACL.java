b28e290966d523239c08f031d39079843d6242a4
NiuhiPlays <nielsiansmeets@gmail.com>
2025-10-19 23:41:41 +0200

    Made a Basic start for YACL Config Screen. Next is to add all the config options so this is completed

--- src/client/java/com/niuhi/config/YACL.java
+++ src/client/java/com/niuhi/config/YACL.java
@@ -1,4 +1,22 @@
 package com.niuhi.config;
 
+import com.niuhi.ElytraRevampedReReWritten;
+import dev.isxander.yacl3.api.YetAnotherConfigLib;
+import net.minecraft.client.gui.screen.Screen;
+
 public class YACL {
+
+    public static Screen getConfigScreen(Screen parent) {
+    if (!ElytraRevampedReReWritten.YACL_LOADED) {
+        ElytraRevampedReReWritten.LOGGER.info("YACL not loaded");
+        return null;
+    }
+
+    YetAnotherConfigLib.Builder builder = YetAnotherConfigLib.createBuilder();
+
+
+
+    Screen screen = builder.build().generateScreen(parent);
+    return screen;
+    }
 }
