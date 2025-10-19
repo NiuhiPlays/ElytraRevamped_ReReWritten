f6dcdcd66112f664221da96da7b5ebbaeb89f9ea
NiuhiPlays <nielsiansmeets@gmail.com>
2025-10-19 23:41:07 +0200

    Added ModMenu API

--- src/client/java/com/niuhi/config/ModMenu.java
+++ src/client/java/com/niuhi/config/ModMenu.java
@@ -1,4 +1,35 @@
 package com.niuhi.config;
 
-public class ModMenu {
+import com.niuhi.ElytraRevampedReReWritten;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+
+public class ModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+
+        return parent -> {
+            if (ElytraRevampedReReWritten.YACL_LOADED) {
+                return YACL.getConfigScreen(parent);
+            }
+
+            return new Screen(Text.literal("Config Unavailable")) {
+                @Override
+                protected void init() {
+                    this.addDrawableChild(ButtonWidget.builder(
+                                    Text.literal("YACL Not Installed, Use JSON file"),
+                                    button -> {
+                                        this.close();
+                                    }
+                            )
+                            .position(this.width / 2 - 100, this.height / 2)
+                            .size(200, 20)
+                            .build());
+                }
+            };
+        };
+    }
 }
